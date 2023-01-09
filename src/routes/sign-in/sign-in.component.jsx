40ffc6d9bd52ea85d78f95c4294a6816472ac011
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2023-01-09 09:17:33 +0100

    init redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase";
+
 import "./sign-in.styles.css";
+
 function SignIn() {
+  //
+  useEffect(() => {
+    const fun = async () => {
+      const res = await getRedirectResult(auth);
+      const userdocref = await createUserDocumentFromAuth(res.user);
+
+      console.log(res);
+    };
+    fun();
+  }, []);
   const logWithGoogle = async () => {
+    //
     const { user } = await signInWithGooglePopup();
     const userdocref = await createUserDocumentFromAuth(user);
     console.log(userdocref);
   };
+  //
   return (
     <div className="google-sign-in-button">
+      {/*  */}
       <button onClick={logWithGoogle} type="button">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
@@ -19,6 +38,15 @@ function SignIn() {
         />
         <b>Google</b>
       </button>
+      {/*  */}
+      <button onClick={signInWithGoogleRedirect} type="button">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+          alt=""
+        />
+        <b>Google_Redirect</b>
+      </button>
+      {/*  */}
     </div>
   );
 }
