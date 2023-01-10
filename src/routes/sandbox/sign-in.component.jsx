b467b6cd95d1bd017a28309b1a5a56ff7c230a2f
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2023-01-10 09:32:07 +0100

    init sandbox

--- src/routes/sandbox/sign-in.component.jsx
+++ src/routes/sandbox/sign-in.component.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Fragment } from "react";
+import "./sign-in.styles.css";
 function SandBox() {
-  return <div>SandBox</div>;
+  return (
+    <Fragment>
+      <div className="parent">
+        <div className="item-1">1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default SandBox;
