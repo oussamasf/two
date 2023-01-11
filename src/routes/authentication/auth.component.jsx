cff4316057c31da519c071eac166f401d2f8be07
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2023-01-11 13:58:35 +0100

    comment useless coded

--- src/routes/authentication/auth.component.jsx
+++ src/routes/authentication/auth.component.jsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 import { getRedirectResult } from "firebase/auth";
 import {
   auth,
-  signInWithGooglePopup,
-  signInWithGoogleRedirect,
+  // signInWithGooglePopup,
+  // signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase";
-import Button from "../../components/button/button.component";
+// import Button from "../../components/button/button.component";
 import "./auth.styles.css";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
@@ -15,20 +15,19 @@ import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 function Auth() {
   //
   useEffect(() => {
-    const fun = async () => {
+    (async () => {
       const res = await getRedirectResult(auth);
       await createUserDocumentFromAuth(res.user);
 
       console.log(res);
-    };
-    fun();
+    })();
   }, []);
-  const logWithGoogle = async () => {
-    //
-    const { user } = await signInWithGooglePopup();
-    const userdocref = await createUserDocumentFromAuth(user);
-    console.log(userdocref);
-  };
+  // const logWithGoogle = async () => {
+  //   //
+  //   const { user } = await signInWithGooglePopup();
+  //   const userdocref = await createUserDocumentFromAuth(user);
+  //   console.log(userdocref);
+  // };
   //
   return (
     <div className="auth-container">
